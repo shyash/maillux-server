dd38612f9d83ccd04157603db0762a1be7721c1e
shyash <shyash222@gmail.com>
2020-01-06 00:50:03 +0530

    add subscriber verification

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const {getCourses, addCourse} = require('../controllers/courses')
-const {getSubscribers, addSubscriber} = require('../controllers/subscribers')
+const {getSubscribers, addSubscriber, verifySubscriber} = require('../controllers/subscribers')
 const router = express.Router();
 
 router.route('/').get(getCourses).post(addCourse)
 router.route('/:courseId/subscribers').get(getSubscribers).post(addSubscriber)
+router.route('/:courseId/subscribers/:subscriberId/verify').get(verifySubscriber)
 
 module.exports = router
