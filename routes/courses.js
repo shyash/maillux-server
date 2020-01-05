e8c20b4bcb3cd3759815905d7a889b0af87ae6a4
shyash <shyash222@gmail.com>
2020-01-05 13:06:57 +0530

    add subscriber routes

--- routes/courses.js
+++ routes/courses.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const {getCourses, addCourse} = require('../controllers/courses')
+const {getSubscribers, addSubscriber} = require('../controllers/subscribers')
 const router = express.Router();
 
 router.route('/').get(getCourses).post(addCourse)
+router.route('/:courseId/subscribers').get(getSubscribers).post(addSubscriber)
 
 module.exports = router
