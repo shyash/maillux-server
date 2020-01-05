b58e77e361da466cc63c0e64f3156dc647989642
shyash <shyash222@gmail.com>
2020-01-06 00:49:10 +0530

    refactor

--- controllers/subscribers.js
+++ controllers/subscribers.js
@@ -1,35 +1,65 @@
-const Course = require('../models/Course')
-exports.getSubscribers = async (req,res) => {
+const Course = require('../models/Course');
+const {sendVerificationMail,sendVerificationSuccessfulMail} = require('../config/email/utils')
+
+exports.getSubscribers = async (req, res) => {
     try {
-        const course = await Course.findOne({_id : req.params.courseId});
+        const course = await Course.findOne({ _id: req.params.courseId });
         return res.status(200).json({
-            success : true,
-            count : course.subscribers.length,
-            data : course.subscribers
-        })
+            success: true,
+            count: course.subscribers.length,
+            data: course.subscribers
+        });
     } catch (err) {
-        console.log(err)
-        res.status(500).json({ error : 'Server Error'})
+        console.log(err);
+        res.status(500).json({ error: 'Server Error' });
     }
-} 
+};
 
-exports.addSubscriber = async (req,res) => {
-        try {
-            const course = await Course.findOne({_id : req.params.courseId});
-            let subscriber = {
-                name : req.body.name,
-                email : req.body.email
+exports.addSubscriber = async (req, res) => {
+    try {
+        const course = await Course.findOne({ _id: req.params.courseId });
+        const subscriber = {
+            name: req.body.name,
+            email: req.body.email
+        };
+        course.subscribers.push(subscriber);
+       
+        const info = await sendVerificationMail(
+            course,
+            course.subscribers[course.subscribers.length - 1]
+        );
+        course.save();
+        console.log(info);
+        return res.status(201).json({
+            success: true,
+            data: course.subscribers[course.subscribers.length - 1]
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: 'Server Error' });
+    }
+};
+
+exports.verifySubscriber = async (req, res) => {
+    try {
+        const course = await Course.findOne({ _id: req.params.courseId });
+        let s = 0;
+        while (s < course.subscribers.length) {
+            if (course.subscribers[s]._id == req.params.subscriberId) {
+                if (course.subscribers[s].isVerified) throw 'Already verified';
+                course.subscribers[s].isVerified = true;
+                break;
             }
-            course.subscribers.push(subscriber)
-            course.save()
-            return res.status(201).json({
-                success : true,
-                data : course.subscribers[course.subscribers.length-1]
-            })
-        } catch (err) {
-            console.log(err)
-            res.status(500).json({ error : 'Server Error'})
+            s++;
         }
-    
-}
-
+        if (s == course.subscribers.length) throw 'Subscriber not found!';
+        const info = await sendVerificationSuccessfulMail(course,course.subscribers[s])
+        console.log(info)
+        course.save();
+        //do first mail
+        return res.end('Email verification Successful!');
+    } catch (error) {
+        console.log(error)
+        return res.end(error);
+    }
+};
