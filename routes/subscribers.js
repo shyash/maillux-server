7fc464db3bb0bf42c5b0ad7bfaa21bff99c8b6f6
shyash <shyash222@gmail.com>
2020-01-05 13:06:30 +0530

    delete subscriber routes

--- routes/subscribers.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const {getCourses, addCourse} = require('../controllers/subscribers')
-const router = express.Router();
-
-router.route('/').get(getSubscribers).post(addSubscriber).put(verifySubscriber)
-
-module.exports = router
