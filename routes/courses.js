8159e6d2abd1dbf21df0ffcc7a0b69388551b70c
shyash <shyash222@gmail.com>
2020-01-09 23:22:42 +0530

    add edit material route

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const {getCourses, addCourse} = require('../controllers/courses')
+const {getCourses, addCourse, publishCourse, getCourse, editMaterial} = require('../controllers/courses')
 const {getSubscribers, addSubscriber, verifySubscriber} = require('../controllers/subscribers')
 const router = express.Router();
 
 router.route('/').get(getCourses).post(addCourse)
+router.route('/:courseId/').get(getCourse).post(publishCourse).put(editMaterial)
 router.route('/:courseId/subscribers').get(getSubscribers).post(addSubscriber)
 router.route('/:courseId/subscribers/:subscriberId/verify').get(verifySubscriber)
 
