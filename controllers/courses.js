5614eec92907a35a33b1b0df9914fa42bfd39191
shyash <shyash222@gmail.com>
2020-01-09 23:22:58 +0530

    add controller for edit material

--- controllers/courses.js
+++ controllers/courses.js
@@ -18,10 +18,12 @@ exports.getCourses = async (req,res,next) => {
 exports.addCourse = async (req,res) => {
     const token = req.headers['x-access-token']
     let decodedRes = await verifyJWT(token)
+    console.log(decodedRes)
     if(decodedRes.error) res.json(decodedRes)
     else{
         try {
             const fetchedUser = await User.findById(decodedRes.id);
+            console.log(fetchedUser)
             const course = await Course.create(req.body.content)
             console.log(course)
             fetchedUser.courses.push(course)
@@ -37,7 +39,63 @@ exports.addCourse = async (req,res) => {
     }
 }
 
-
-
+exports.publishCourse = async (req,res) => {
+    const token = req.headers['x-access-token']
+    let decodedRes = await verifyJWT(token)
+    if(decodedRes.error) res.json(decodedRes)
+    else{
+        try {
+            
+            const course = await Course.findById(req.params.courseId)
+            course.isPublished = true
+            course.save()
+            return res.status(201).json({
+                success : true,
+                data : course
+            })
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({ error : 'Server Error'})
+        }
+    }
+}
+exports.getCourse = async (req,res) => {
+    const token = req.headers['x-access-token']
+    let decodedRes = await verifyJWT(token)
+    if(decodedRes.error) res.json(decodedRes)
+    else{
+        try {
+            const course = await Course.findById(req.params.courseId) 
+            return res.status(201).json({
+                success : true,
+                data : course
+            })
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({ error : 'Server Error'})
+        }
+    }
+}
+exports.editMaterial = async (req,res) => {
+    const token = req.headers['x-access-token']
+    let decodedRes = await verifyJWT(token)
+    if(decodedRes.error) res.json(decodedRes)
+    else{
+        try {
+            const course = await Course.findById(req.params.courseId)
+            course.content[req.body.day -1 ][0].material = req.body.material
+            course.markModified('content');
+            const i = await course.save()
+            console.log(i.content)
+            return res.status(201).json({
+                success : true,
+                data : i
+            })
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({ error : 'Server Error'})
+        }
+    }
+}
 
 
