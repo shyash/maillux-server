53ebf3d2356ce5f85aec709a48f4711ab237e88a
shyash <shyash222@gmail.com>
2020-01-12 23:05:25 +0530

    add routine mail

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const path = require('path');
-const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const connectDB = require('./config/db')
-dotenv.config({path : './config/config.env'})
-const app = express()
-app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(cors())
-connectDB()
-const PORT = process.env.PORT || 8800
-app.use('/api/courses',require('./routes/courses'))
-app.use('/api/user',require('./routes/user'))
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+dotenv.config({ path: './config/config.env' });
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+connectDB();
+const PORT = process.env.PORT || 8800;
+app.use('/api/courses', require('./routes/courses'));
+app.use('/api/user', require('./routes/user'));
+const { routineMail } = require('./config/email/routineMail');
+// console.log(routineMail());
+// (async()=>{
+//     setTimeout(() => {
 
-app.listen(PORT,()=>console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+//     }, timeout);
+// })
+app.listen(PORT, () =>
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+);
