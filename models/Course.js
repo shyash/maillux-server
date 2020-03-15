8cb45845e53e0e3d48b5a434c02cf093e5b289c9
shyash <shyash222@gmail.com>
2020-03-15 19:59:11 +0530

    update content type

--- models/Course.js
+++ models/Course.js
@@ -1,54 +1,54 @@
-const mongoose = require('mongoose');
-const { SubscriberSchema } = require('../models/Subscriber');
+const mongoose = require("mongoose");
+const { SubscriberSchema } = require("../models/Subscriber");
 const CourseSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, 'Please add the course title'],
-        unique: false,
-        trim: true
-    },
-    isFirstSave: {
-        type: Boolean,
-        default: true
-    },
-    duration: {
-        type: Number,
-        required: [true, 'Please add the course duration (days)']
-    },
-    content: {
-        type: []
-    },
-    isPublished: {
-        type: Boolean,
-        default: false
-    },
-    subscribers: [SubscriberSchema],
-    author: {
-        type: String,
-        default: ''
-    },
-    description: {
-        type: String
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        select: false
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  title: {
+    type: String,
+    required: [true, "Please add the course title"],
+    unique: false,
+    trim: true
+  },
+  isFirstSave: {
+    type: Boolean,
+    default: true
+  },
+  duration: {
+    type: Number,
+    required: [true, "Please add the course duration (days)"]
+  },
+  content: {
+    type: []
+  },
+  isPublished: {
+    type: Boolean,
+    default: false
+  },
+  subscribers: [SubscriberSchema],
+  author: {
+    type: String,
+    default: ""
+  },
+  description: {
+    type: String
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    select: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-CourseSchema.pre('save', async function(next) {
-    console.log(this.isFirstSave);
-    if (this.isFirstSave) {
-        this.content = Array(this.duration).fill([{ material: 'hey wow' }]);
-        this.isFirstSave = false;
-    }
+CourseSchema.pre("save", async function(next) {
+  console.log(this.isFirstSave);
+  if (this.isFirstSave) {
+    this.content = Array(this.duration).fill({ title: "", material: "" });
+    this.isFirstSave = false;
+  }
 
-    next();
+  next();
 });
 
-module.exports = mongoose.model('Course', CourseSchema);
+module.exports = mongoose.model("Course", CourseSchema);
