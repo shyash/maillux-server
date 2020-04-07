f281e15131b54290a2708a804c2a56af791f42a4
shyash <shyash222@gmail.com>
2020-04-07 16:11:02 +0530

    format mail

--- config/email/utils.js
+++ config/email/utils.js
@@ -7,16 +7,16 @@ you have successfully subscribed to ${course.title}, <br/> You will be receiving
 };
 
 exports.sendVerificationMail = async (course, subscriber) => {
-  const content = `Hey ${subscriber.name},<br/> Thanks for signing up for ${course.title} <br/> The course duration is ${course.duration} days.<br/><br/>${course.description}<br/><br/><a href="http://127.0.0.1:8800/api/courses/${course._id}/subscribers/${subscriber._id}/verify">Verify your email</a> first to get access to the course content.`;
+  const content = `Hey ${subscriber.name},<br/> Thanks for signing up for ${course.title} <br/> The course duration is ${course.duration} days.<br/><br/>${course.description}<br/><br/><a href="https://maillux.herokuapp.com/api/courses/${course._id}/subscribers/${subscriber._id}/verify">Verify your email</a> first to get access to the course content.`;
   return postmail(subscriber.email, `Maillux : ${course.title}`, content);
 };
 
 exports.sendFirstMail = async (course, subscriber) => {
   let content;
   course.content[subscriber.position - 1].wrap
-    ? (content = `<pre>${
+    ? (content = `<pre style="font-family:inherit"><code style="font-family:inherit">${
         course.content[subscriber.position - 1].material
-      }</pre>`)
+      }</code></pre>`)
     : (course = course.content[subscriber.position - 1].material);
   const subject = `Maillux : Day 1 ${course.title} | ${
     course.content[subscriber.position - 1].material
